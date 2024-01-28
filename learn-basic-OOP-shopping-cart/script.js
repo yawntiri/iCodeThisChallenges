7357b1156be0df74b2bace110dc2e509c1aff67e
Yaw Ntiri <yawntiri33@gmail.com>
2024-01-28 23:40:08 +0000

    update on shoppingcart button

--- learn-basic-OOP-shopping-cart/script.js
+++ learn-basic-OOP-shopping-cart/script.js
@@ -141,8 +141,10 @@ class ShoppingCart {
 
   calculateTotal() {
     const subTotal = this.items.reduce((total, item) => total + item.price, 0);
-  }
-
+  const tax = this.calculateTaxes(subTotal);
+  this.total = subTotal + tax;
+  cartSubTotal.textContent = `$${subTotal.toFixed(2)}`;
+}
   };
 
   
