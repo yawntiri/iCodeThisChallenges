de5eff195935d0196944f878bf1dd897f17dfdc7
Yaw Ntiri <yawntiri33@gmail.com>
2024-02-04 22:44:02 +0000

    first initial commit

---
+++ buildPlatformerGame-OOP/script.js
@@ -0,0 +1,10 @@
+const startBtn = document.getElementById("start-btn");
+const canvas = document.getElementById("canvas");
+const startScreen = document.querySelector(".start-screen");
+const checkpointScreen = document.querySelector(".checkpoint-screen");
+const checkpointMessage = document.querySelector(".checkpoint-screen > p");
+const ctx = canvas.getContext("2d");
+
+
+canvas.width
+
