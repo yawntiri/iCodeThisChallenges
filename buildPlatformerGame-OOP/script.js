e77178003ecea16f3412da53e098f07b130b6dfd
Yaw Ntiri <yawntiri33@gmail.com>
2024-02-20 21:33:42 +0000

    added a platform class

--- buildPlatformerGame-OOP/script.js
+++ buildPlatformerGame-OOP/script.js
@@ -50,6 +50,24 @@ class Player {
   }
 }
 
+class Platform{
+  constructor(x, y, width, height) {
+    this.position = {
+      x,
+      y,
+    };
+    this.width = width;
+    this.height = height;
+  }
+  draw() {
+    ctx.fillStyle = "#777777";
+    ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+  update() {
+    this.draw();
+  }
+}
+
 const player = new Player();
 
 
